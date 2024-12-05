544d2bd2a45f72491b1dbc5c2f43804f02a32f47
Endriw Ventura <endriwventura@yahoo.com>
2024-12-05 20:16:56 -0300

    Adicionar arquivos de projeto.

---
+++ MoviesAPI/Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MoviesAPI.Models;
+
+namespace MoviesAPI.Controllers;
+
+[ApiController]
+[Route("Movies")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+    private static int id = 0;
+
+    [HttpPost]
+    public IActionResult AddMovie([FromBody] Movie movie)
+    {
+        movie.Id = id++;
+        movies.Add(movie);
+        return CreatedAtAction(nameof(GetMovieByID), new { id = movie.Id }, movie);
+    }
+
+    [HttpGet]
+    public IEnumerable<Movie> GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 20)
+    {
+        return movies.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetMovieByID(int id)
+    {
+        Movie? movie = movies.FirstOrDefault(m => m.Id == id);
+        if (movie == null)
+            return NotFound();
+
+        return Ok(movie);
+    }
+
+
+}
---
+++ MoviesAPI/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesAPI.Models;
+
+public class Movie
+{
+    public int Id { get; internal set; }
+
+    [Required(ErrorMessage = "The Title field is required")]
+    [MaxLength(50, ErrorMessage = "The Title field must be at most 50 characters")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "The Gender field is required")]
+    [MaxLength(50, ErrorMessage = "The Gender field must be at most 50 characters")]
+    public string Gender { get; set; }
+
+    [Required(ErrorMessage = "The Duration field is required")]
+    [Range(45, 400, ErrorMessage = "The Duration field must be between 45 and 400 minutes")]
+    public int Duration { get; set; }
+
+}
