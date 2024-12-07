fe879002ec2bc8fa60e4d8852c70e5d442490b19
Endriw Ventura <endriwventura@yahoo.com>
2024-12-07 20:54:55 -0300

    #Add - Added the Movie class file

--- MoviesAPI/Models/Movie.cs
+++ MoviesAPI/Models/Movie.cs
@@ -4,6 +4,8 @@ namespace MoviesAPI.Models;
 
 public class Movie
 {
+    [Key]
+    [Required]
     public int Id { get; internal set; }
 
     [Required(ErrorMessage = "The Title field is required")]
