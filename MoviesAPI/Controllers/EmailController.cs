d1e90821d5cef4040cf0cbfd782b2ee95a64c98e
Endriw Ventura <endriwventura@yahoo.com>
2025-06-07 16:06:51 -0300

    #Feat. Alteração das controllers

--- MoviesAPI/Controllers/EmailController.cs
+++ MoviesAPI/Controllers/EmailController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MoviesAPI.Data.DTOs;
-using MoviesAPI.Models;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data.DTOs.EmailReset;
 using MoviesAPI.Services;
 
 namespace MoviesAPI.Controllers
@@ -10,10 +10,12 @@ namespace MoviesAPI.Controllers
     public class EmailController : ControllerBase
     {
         private readonly EmailService _emailService;
+        private readonly UserService _userService;
 
-        public EmailController(EmailService emailService)
+        public EmailController(EmailService emailService, UserService userService)
         {
             _emailService = emailService;
+            _userService = userService;
         }
 
         [HttpPost]
@@ -21,13 +23,40 @@ namespace MoviesAPI.Controllers
         {
             try
             {
-                _emailService.EnviarEmail(address);
+                string subject = "Password Recovery";
+                var token = GeneratePasswordResetToken(address);
+
+                if (token == null)
+                    throw new Exception("Erro ao gerar o Token");
+
+                var link = GenerateResetLink(token);
+                var body = $"Clique aqui para redefinir sua senha: <a href=\"{link}\">Redefinir Senha</a>";
+                _emailService.EnviarEmail(address, subject, body);
                 return Ok("Email enviado com sucesso!");
             }
             catch (Exception ex)
             {
                 return StatusCode(500, $"Erro ao enviar e-mail: {ex.Message}");
             }
+        }
+
+        private string? GeneratePasswordResetToken(object address)
+        {
+            var user = _userService.FindByEmail(address);
+            if (user == null)
+                return null;
+
+            var token = Guid.NewGuid().ToString();
+
+            _emailService.SavePasswordResetToken(user.Id, token, DateTime.UtcNow.AddHours(1));
+
+            return token;
+        }
+
+        public string GenerateResetLink(string token)
+        {
+            var baseUrl = "localhost:3000/redefinir-senha"; 
+            return $"{baseUrl}?token={token}";
         }
     }
 }
---
+++ MoviesAPI/Controllers/ResetController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Data.DTOs.EmailReset;
+using MoviesAPI.Services;
+
+namespace MoviesAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("Reset")]
+    public class ResetController : ControllerBase
+    {
+        private readonly EmailService _emailService;
+        private readonly UserService _userService;
+
+
+        public ResetController(EmailService emailService, UserService userService)
+        {
+            _emailService = emailService;
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public IActionResult RedefinirSenha([FromBody] ResetPasswordDto dto)
+        {
+            var tokenInfo = _emailService.GetPasswordResetToken(dto.Token);
+
+            if (tokenInfo == null || tokenInfo.expire < DateTime.UtcNow)
+                return BadRequest("Invalid or expired token");
+
+            var user = _userService.GetUserByID(tokenInfo.UserID);
+            if (user == null)
+                return NotFound();
+
+            _userService.EditPasswordReset(user, dto.NewPassword);
+
+            var success = _emailService.DeleteToken(dto.Token);
+            if (!success)
+                return BadRequest("Invalid or expired token");
+
+            return Ok("Password reset successfully!");
+        }
+    }
+}
